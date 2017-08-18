4800209518b0aa312a7496ff613a8fcfbd1abe12
Anna Dolan <anna.dolan115@gmail.com>
2017-08-17 20:46:17 -0500

    added javacript

---
+++ cards.js
@@ -0,0 +1,26 @@
+var getCreateCard = document.getElementById('cardHolderDiv');
+var getText = document.getElementById('text');
+var getCreate = document.getElementById('create');
+
+
+function domString(){
+	var cardString = '';
+	cardString += `<div class="deleteButtonCard">`
+	cardString += `<p>${getText.value}</p>`
+	cardString += `<button class="delete" id="delete">x</button>`
+	cardString += `</div>`
+	getCreateCard.innerHTML = cardString;
+	console.log(cardString);
+}
+
+function deleteButton(e){
+	if(e.target.className === 'delete'){
+		e.target.parentElement.remove();
+	}
+	console.log(e);
+}
+
+
+
+getCreate.addEventListener('click', domString);
+document.body.addEventListener('click', deleteButton);
